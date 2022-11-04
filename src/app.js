effde8e7314486c50e53dce3fc43f8c78a770b87
Luis Manuel Palmera Quintero <dfguerrero@unicesar.edu.co>
2022-11-03 19:56:18 -0500

    3 de noviembre
    3

--- src/app.js
+++ src/app.js
@@ -10,6 +10,17 @@ app.get('/api/products', (req, res) => {
     res.send(datafull.products);
 })
 
+app.get('api/products/slug/:slug', (req, res) => {
+    const product = datafull.products.find( (x) => x.slug === req.params-slug);
+    if(product) {
+        res.send(product);
+    }
+    else
+    {
+        res.status(404).send({message: 'Producto no encontrado'});
+    }
+});
+
 //Configuraciones
 app.set('port', process.env.PORT || 5000)
 
